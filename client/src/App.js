893d2dbb18412644b2c4be175aae3d49516da095
FengyangZhang <fz2ds@virginia.edu>
2018-12-06 17:35:15 -0500

    add menu to organize the app

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
 import AddPanel from "./AddPanel";
 import SongList from "./SongList";
+import {Link} from 'react-router-dom';
 
 class App extends Component {
     render() {
         return (
             <div className="APP">
-                <SongList />            
-                <AddPanel />
+                <button className="menuButton"><Link to="/Home">Home</Link></button>
+                <button className="menuButton"><Link to="/SongList">See listed songs</Link></button>
+                <button className="menuButton"><Link to="/AddPanel">Go to add songs</Link></button>
             </div>
         );
     }
--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {
+    BrowserRouter as Router,
+    Route,
+} from 'react-router-dom';
+import AddPanel from "./AddPanel";
+import SongList from "./SongList";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routes = (
+    <Router>
+        <div>
+        <Route path="/" component={App} />
+        <Route path = "/SongList" component = {SongList}/>
+        <Route path = "/AddPanel" component = {AddPanel}/>
+        </div>
+    </Router>
+  );
+ReactDOM.render(routes, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
