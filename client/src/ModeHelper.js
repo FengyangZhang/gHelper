20758f2683d0ef5d58019002059633b07f2b7519
FengyangZhang <fz2ds@virginia.edu>
2018-12-09 20:33:55 -0500

    start working on mode helper

--- client/src/ModeHelper.js
+++ client/src/ModeHelper.js
@@ -2,10 +2,21 @@ import React, { Component } from "react";
 require('./css/ModeHelper.css');
 
 export default class ModeHelper extends Component {
+
+    modeToPitches(mode) {
+        const isMajor = mode.indexOf("大") !== -1 ? 1 : 0;
+        const modeName = isMajor ? mode.substring(0, mode.indexOf("大")) 
+            : mode.substring(0, 1).toUpperCase()+mode.substring(1, mode.indexOf("小"));
+        const intervals = isMajor ? [2, 2, 1, 2, 2, 2, 1] : [2, 1, 2, 2, 1, 2, 2];
+        const notes = ["C", "C#/Db", "D", "D#/Eb", "E", "F", "#F/Gb", "G", "G#/Ab", "A", "A#/Bb", "B"];
+        return <i>{modeName}</i>;
+    }
+
+
     render() {
         return (
             <div className="modeHelperDiv">
-                {this.props.mode}，构成音如下:
+                {this.props.mode}，构成音如下: {this.modeToPitches(this.props.mode)}
             </div>
         );
     }
