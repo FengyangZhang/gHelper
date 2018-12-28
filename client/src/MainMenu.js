797ec72db8c1d6593b0baf7420b3d56ae4285993
FengyangZhang <fz2ds@virginia.edu>
2018-12-28 16:44:34 -0500

    add style to menu bar

--- client/src/MainMenu.js
+++ client/src/MainMenu.js
@@ -1,13 +1,20 @@
 import React, { Component } from "react";
 import {Link} from 'react-router-dom';
+require('./css/MainMenu.css');
 
 class MainMenu extends Component {
     render() {
         return (
             <div className="mainMenu">
-                <button className="menuButton"><Link to="/">Home</Link></button>
-                <button className="menuButton"><Link to="/songlist">See listed songs</Link></button>
-                <button className="menuButton"><Link to="/addpanel">Go to add songs</Link></button>
+                <button className="menuButton">
+                    <Link to="/" style={{ textDecoration: 'none', color: 'white'}}>Home</Link>
+                </button>
+                <button className="menuButton">
+                    <Link to="/songlist" style={{ textDecoration: 'none', color: 'white'}}>See listed songs</Link>
+                </button>
+                <button className="menuButton">
+                    <Link to="/addpanel" style={{ textDecoration: 'none', color: 'white'}}>Go to add songs</Link>
+                </button>
             </div>
         );
     }
